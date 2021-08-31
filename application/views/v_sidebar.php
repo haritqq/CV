2d7699c37c5676407601ebd45e506d27dd0739f7
haritqq <muhammad.harit@gmail.com>
2021-08-31 10:48:19 +0700

    add assets boxicons

---
+++ application/views/v_sidebar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--=========== ICON SIDEBAR ============-->
+
+
+    <!--============ CSS ============-->
+    <link rel="stylesheet" href="">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <!--============ HEADER ==========-->
+    <header class="header">
+
+    </header>
+
+    <!--============ NAVBAR ===========-->
+    <div class="nav" id="navbar">
+
+    </div>
+
+    <!--============ CONTENTS ===========-->
+
+
+    <!--=========== MAIN JS ===========-->
+    <script src=""></script>
+</body>
+
+</html>
